96bfe921c6b372ed833b8b2d21f604dbc001c0aa
Shubham Maurya <code.shaurya@gmail.com>
2018-03-04 10:23:08 +0530

    Bit manipulation

---
+++ BitManipulation/maximum-and.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll t,a,b;
+	cin>>t;
+	while(t--){
+		cin>>a>>b;
+		
+	}
+	return 0;
+}
+
+/**
+https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/maximum-and/
+*/
